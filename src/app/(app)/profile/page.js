226eb7ec0e39218db644eb6e7af3f7e7f88b5be2
ronykader <rony.ksr06@gmail.com>
2024-08-29 12:12:52 +0600

    update - log checking

--- src/app/(app)/profile/page.js
+++ src/app/(app)/profile/page.js
@@ -6,11 +6,10 @@ import React, { useEffect, useState } from 'react'
 
 const page = () => {
     const { user } = useAuth({ middleware: 'auth' })
-    console.log(user, 'checking')
     const [profileDetails, setProfileDetails] = useState()
+    console.log(profileDetails)
     useEffect(() => {
         axios.get(`/api/profile-data/${user.id}`).then(response => {
-            console.log(response, 'checking')
             setProfileDetails(response?.data)
         })
     }, [])
@@ -18,15 +17,15 @@ const page = () => {
         <div className="py-12">
             <div className="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
                 <div className="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                    {profileDetails ? (
-                        <UpdateSignature
-                            user={user}
-                            profileSignature={profileDetails?.signature}
-                            className="max-w-xl"
-                        />
-                    ) : (
-                        ''
-                    )}
+                    {/*{profileDetails ? (*/}
+                    {/*    <UpdateSignature*/}
+                    {/*        user={user}*/}
+                    {/*        profileSignature={profileDetails?.signature}*/}
+                    {/*        className="max-w-xl"*/}
+                    {/*    />*/}
+                    {/*) : (*/}
+                    {/*    ''*/}
+                    {/*)}*/}
                 </div>
             </div>
         </div>
